bcf8a0729c8e97cc0e420247fdccdf0fa81caf69
anandayush81 <anandayush81@gmail.com>
2023-10-22 01:39:59 +0530

    Updates Code

---
+++ src/main/java/com/lld3/parkinglotdesign/models/BaseModel.java
@@ -0,0 +1,16 @@
+package com.lld3.parkinglotdesign.models;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.experimental.SuperBuilder;
+
+import java.util.Date;
+
+@SuperBuilder
+@Setter
+@Getter
+public abstract class BaseModel {
+    private Long id;
+    private Date createdAt;
+    private Date updatedAt;
+}
